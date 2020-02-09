43747d183e62521942a369ee7f9c2bac099ee5bb
kelicom <735579768@qq.com>
2020-02-09 23:58:30 +0800

    优化读取方法

--- controller/admin/Addons.php
+++ controller/admin/Addons.php
@@ -30,13 +30,20 @@ class Addons extends Base
             'price'     => 0,
             'qq'        => '',
         ];
-        $addonsPath = App::getAppPath() . '/addons/' . $info['name'];
-        if (is_dir($addonsPath)) {
-            $this->error('插件目录已经存在');
-        }
         $temAddonsPath = App::getRuntimePath() . '/addons/' . $info['name'] . '.zip';
         is_dir(dirname($temAddonsPath)) or mkdir(dirname($temAddonsPath), 777, true);
         is_file($temAddonsPath) or file_put_contents($temAddonsPath, file_get_contents('http://www.zkeli.com/comment.zip'));
+        $addonsInfo = $this->readZipFile($temAddonsPath, 'composer.json');
+        if ($addonsInfo === false) {
+            $this->error('此插件格式不正确');
+        }
+        $addonsInfo = json_decode($addonsInfo, true);
+        //判断插件是不是已经安装
+        $addonsPath = App::getAppPath() . '/addons/' . $addonsInfo['name'];
+        if (is_dir($addonsPath)) {
+            $this->error('插件目录已经存在');
+        }
+
         $zip     = new ZipArchive();
         $openRes = $zip->open($temAddonsPath);
         if ($openRes === true) {
@@ -70,5 +77,35 @@ class Addons extends Base
     public function uninstall()
     {
         # code...
+    }
+
+    /**
+     * 读取压缩包内文件
+     * @authname [name]     0
+     * @DateTime 2020-02-09
+     * example readZipFile('E:/1.zip','composer.json')
+     * @Author   mokuyu
+     *
+     * @param  string   $zipPath  压缩包全路径
+     * @param  string   $filePath 压缩包内文件相对路径
+     * @return [type]
+     */
+    private function readZipFile($zipPath = '', $filePath = '')
+    {
+        $file_protocol = 'zip://' . $zipPath . '#' . $filePath;
+
+        $fp = fopen($file_protocol, 'rb');
+
+        if (!$fp) {
+            return false;
+        }
+        $content = '';
+        while (!feof($fp)) {
+            $content .= fread($fp, 1024);
+        }
+
+        fclose($fp);
+
+        return $content;
     }
 }
