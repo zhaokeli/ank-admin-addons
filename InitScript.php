48ff1ac5b9345ee8026a0eb775dfbafafc7ea57c
kelicom <735579768@qq.com>
2020-02-12 20:39:52 +0800

    add InitScript.php

---
+++ InitScript.php
@@ -0,0 +1,33 @@
+<?php
+namespace ank\adminaddons;
+
+/**
+ * 初始化脚本
+ */
+class InitScript extends \utils\admin\InitScript
+{
+    private $data = [
+        'url'        => 'Addons/lis',
+        'title'      => '插件管理',
+        'icon_class' => 'ank-icon ank-icon-plugin',
+    ];
+
+    public function install(): bool
+    {
+        $this->addMenu($this->data);
+
+        return true;
+    }
+
+    public function unInstall(): bool
+    {
+        $this->deleteMenu($this->data);
+
+        return true;
+    }
+
+    public function update(): bool
+    {
+        return true;
+    }
+}
